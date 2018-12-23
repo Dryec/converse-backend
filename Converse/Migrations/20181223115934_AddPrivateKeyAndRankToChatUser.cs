43b6bd7d5b939421442edc301782a1ac9e695201
Pascal Akhidue <pascal_akhidue@hotmail.com>
2018-12-23 13:05:28 +0100

    Added Migration.

---
+++ Converse/Migrations/20181223115934_AddPrivateKeyAndRankToChatUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Converse.Migrations
+{
+    public partial class AddPrivateKeyAndRankToChatUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrivateKey",
+                table: "chatusers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "chatusers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrivateKey",
+                table: "chatusers");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "chatusers");
+        }
+    }
+}
